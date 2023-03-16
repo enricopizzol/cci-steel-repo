32b30035f95ada4632af521a871800120e338198
enrico <enrico.pizzol@gmail.com>
2023-03-16 13:04:21 -0300

    add scripts

---
+++ scripts/main.py
@@ -0,0 +1,76 @@
+import csv
+
+CSV_FILENAME = "pads.csv"
+INPUT = "input"
+OUTPUT = "output"
+SIZE = 3
+
+
+def generate_pads():
+    with open(CSV_FILENAME) as f:
+        reader = csv.reader(f)
+        inputs = list(tuple(line) for line in reader)
+
+    file = open("pads_code.txt", "w")
+    signals_file_output = open("signals_output.txt", "w")
+    signal_file_input = open("signals_input.txt", "w")
+
+    for _input in inputs:
+        if _input[2] == INPUT:
+            signal_file_input.write("{pin},{pin_I},\n".format(pin=_input[0], pin_I=_input[1]))
+            if 1 == int(_input[SIZE]):
+                # signal_file_input.write("input {pin},{pin_I};\n".format(pin=_input[0], pin_I=_input[1]))
+                file.write("ICP PAD_{pin}_i(.PAD({pin}),.Y({pin_I}));\n".format(pin=_input[0], pin_I=_input[1]))
+            if int(_input[SIZE]) > 1:
+                for x in range(0, int(_input[SIZE])):
+                    file.write("ICP PAD_{pin}{index}_i(.PAD({pin}[{index}]),.Y({pin_I}[{index}]));\n".format(pin=_input[0],
+                                                                                                      pin_I=_input[1],
+                                                                                                      index=x))
+        if _input[2] == OUTPUT:
+            signals_file_output.write("{pin},{pin_I},\n".format(pin=_input[0], pin_I=_input[1]))
+            if 1 == int(_input[SIZE]):
+                # signals_file_output.write("output {pin},{pin_I};\n".format(pin=_input[0], pin_I=_input[1]))
+                file.write("BD8P PAD_{pin}_o(.A({pin}),.PAD({pin_I}));\n".format(pin=_input[1], pin_I=_input[0])
+                           )
+            if int(_input[SIZE]) > 1:
+                for x in range(0, int(_input[SIZE])):
+                    # signals_file_output.write(
+                    #     "output {pin}{index},{pin_I}{index};\n".format(pin=_input[0], pin_I=_input[1], index=x))
+                    file.write("BD8P PAD_{pin}{index}_o(.A({pin}[{index}]),.PAD({pin_I}[{index}]));\n".format(pin=_input[1],
+                                                                                                       pin_I=_input[0],
+                                                                                                       index=x))
+
+    file.close()
+    signal_file_input.close()
+    signals_file_output.close()
+
+
+def generate_iopads():
+    with open(CSV_FILENAME) as f:
+        reader = csv.reader(f)
+        inputs = list(tuple(line) for line in reader)
+
+    file = open("iopads.txt", "w")
+    for _input in inputs:
+        if _input[2] == INPUT:
+            if 1 == int(_input[SIZE]):
+                file.write("Pad: IOPADS_INST/PAD_bus2ip_wrce_i_2 N ICP;\n".format(pin=_input[0], pin_I=_input[1]))
+            if int(_input[SIZE]) > 1:
+                for x in range(0, int(_input[SIZE])):
+                    file.write("ICP PAD_{pin}_i(.PAD({pin}[{index}]),.Y({pin_I}[{index}]));\n".format(pin=_input[0], pin_I=_input[1],index=x))
+        if _input[2] == OUTPUT:
+            if 1 == int(_input[SIZE]):
+                file.write("BD8P PAD_{pin}_o(.A({pin}),.PAD({pin_I}));\n".format(pin=_input[1], pin_I=_input[0])
+                           )
+            if int(_input[SIZE]) > 1:
+                for x in range(0, int(_input[SIZE])):
+                    # signals_file_output.write(
+                    file.write(
+                        "BD8P PAD_{pin}_o(.A({pin}[{index}]),.PAD({pin_I}[{index}]));\n".format(pin=_input[1],
+                                                                                                pin_I=_input[0],
+                                                                                                index=x))
+
+
+if __name__ == '__main__':
+    generate_pads()
+    # generate_iopads()
